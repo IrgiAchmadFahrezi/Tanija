5468f4902fbcb154bab1864adff2f86cb430bd6a
alqonaah <machrusa86@gmail.com>
2024-06-14 08:42:53 +0700

    memperbaiki pada navbar responsif detail-pembelian

--- src/html/detail-pembelian.php
+++ src/html/detail-pembelian.php
@@ -339,5 +339,9 @@ $conn->close();
     }, 500); // 3000 milidetik atau 3 detik
   });
 </script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="/src/scripts/index.js"></script>
 </body>
 </html>
